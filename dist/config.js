1bad48539c45928c0e4d888bccce3d3bbcd881f6
maorkrasner <maorkrasner02@gmail.com>
2024-02-08 12:01:14 +0200

    config

--- dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const config = { server_port: process.env.SERVER_PORT };
-exports.default = config;
