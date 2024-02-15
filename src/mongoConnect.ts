e10151f2f830a69edca3a1901cf51f74fbdddea8
maorkrasner <maorkrasner02@gmail.com>
2024-02-15 13:10:29 +0200

    Add database handling functions

--- src/mongoConnect.ts
+++ src/mongoConnect.ts
@@ -1,23 +1,26 @@
-import { MongoClient, Db } from "mongodb";
-import logger from "./logger"
-import config from "./config";
+import { MongoClient } from "mongodb";
+import logger from "./logger.js"
+import config from "./config.js";
 
 const mongodbUri: string = config.mongo_uri;
 
-const connectToMongoDB = async () => {
+const dbName: string = config.mongo_db_name;
+
+let client: MongoClient;
+
+export const connectToMongoDB = async () => {
     try {
-        const client = new MongoClient(mongodbUri);
+        client = new MongoClient(mongodbUri);
         await client.connect();
         const db = client.db();
         logger.info("Connected to MongoDB");
-        return { db, client };
     } catch (error: any) {
         logger.error("Error connecting to MongoDB:", error.message);
         process.exit(1);
     }
 };
   
-const closeMongoDBConnection = async (client: MongoClient) => {
+export const closeMongoDBConnection = async (client: MongoClient) => {
     if (client) {
         await client.close();
         logger.info("MongoDB connection closed");
@@ -26,4 +29,20 @@ const closeMongoDBConnection = async (client: MongoClient) => {
     return "Connection failed to close"
 };
 
-export { connectToMongoDB, closeMongoDBConnection };
+export const addDataToDb = async (client: MongoClient, collectionName: string, data: any) => {
+    return await client.db(dbName).collection(collectionName).insertOne(data);
+};
+
+export const deleteDataFromDb = async (client: MongoClient, collectionName: string, filter: any) => {
+    await client.db(dbName).collection(collectionName).deleteOne(filter);
+};
+
+export const getDataFromDb = async (client: MongoClient, collectionName: string, filter: any) => {
+    return await client.db(dbName).collection(collectionName).find(filter).toArray();
+};
+
+export const updateDataInDb = async (client: MongoClient, collectionName: string, filter: any, update: any) => {
+    return await client.db(dbName).collection(collectionName).updateOne(filter, { $set: update });
+};
+
+export { client }
