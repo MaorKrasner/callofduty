6895d89a1f35450e509cc195ba37a86bd4874bc1
maorkrasner <maorkrasner02@gmail.com>
2024-02-15 13:12:20 +0200

    Add .env variables to config file

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ import "dotenv/config";
 
 if (['test', 'dev'].includes(process.env.NODE_ENV!)) dotenv.config({path: `.${process.env.NODE_ENV}.env`})
 
-const config  = { server_port: process.env.SERVER_PORT, mongo_uri: process.env.MONGO_URI || ''}; 
+const config = { server_port: process.env.SERVER_PORT, mongo_uri: process.env.MONGO_URI || '', mongo_db_name: process.env.MONGO_DB_NAME || ''}; 
 
 export default config; 
