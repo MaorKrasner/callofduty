d7ff22a5961db23d5deb54b4f30476480ed87254
maorkrasner <maorkrasner02@gmail.com>
2024-02-25 17:05:05 +0200

    Add patch route

--- src/routes/dutyRoutes.ts
+++ src/routes/dutyRoutes.ts
@@ -1,12 +1,13 @@
 import { FastifyInstance } from "fastify";
 
-import { createDuty, deleteDutyById, getDutiesByFilters, getDutyById } from "../controllers/dutyController.js";
+import { createDuty, deleteDutyById, getDutiesByFilters, getDutyById, updateDutyById } from "../controllers/dutyController.js";
 
 const dutyRoutes = async (server: FastifyInstance) => {
     server.post("/duties", createDuty);
     server.get("/duties", getDutiesByFilters);
     server.get("/duties/:id", getDutyById);
     server.delete("/duties/:id", deleteDutyById);
+    server.patch("/duties/:id", updateDutyById);
 };
 
 export default dutyRoutes;
