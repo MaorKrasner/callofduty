fe9f6fdb4c13d24528c57f80341f500805fdd0f0
maorkrasner <maorkrasner02@gmail.com>
2024-02-25 14:52:28 +0200

    Add duty deletion route

--- src/routes/dutyRoutes.ts
+++ src/routes/dutyRoutes.ts
@@ -1,11 +1,12 @@
 import { FastifyInstance } from "fastify";
 
-import { createDuty, getDutiesByFilters, getDutyById } from "../controllers/dutyController.js";
+import { createDuty, deleteDutyById, getDutiesByFilters, getDutyById } from "../controllers/dutyController.js";
 
 const dutyRoutes = async (server: FastifyInstance) => {
     server.post("/duties", createDuty);
     server.get("/duties", getDutiesByFilters);
     server.get("/duties/:id", getDutyById);
+    server.delete("/duties/:id", deleteDutyById);
 };
 
 export default dutyRoutes;
