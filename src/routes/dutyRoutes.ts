fe9b92e8cedc28570566306f16f14b64a5e68b00
maorkrasner <maorkrasner02@gmail.com>
2024-02-25 14:26:24 +0200

    Add route to search duty by id

--- src/routes/dutyRoutes.ts
+++ src/routes/dutyRoutes.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance } from "fastify";
 
-import { createDuty, getDutiesByFilters } from "../controllers/dutyController.js";
+import { createDuty, getDutiesByFilters, getDutyById } from "../controllers/dutyController.js";
 
 const dutyRoutes = async (server: FastifyInstance) => {
     server.post("/duties", createDuty);
     server.get("/duties", getDutiesByFilters);
+    server.get("/duties/:id", getDutyById);
 };
 
 export default dutyRoutes;
