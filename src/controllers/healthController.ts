e3b922d792a6338f54c6fa8709818fe1a3fa775b
maorkrasner <maorkrasner02@gmail.com>
2024-02-28 14:25:56 +0200

    Last fixup

--- src/controllers/healthController.ts
+++ src/controllers/healthController.ts
@@ -4,27 +4,13 @@ import logger from "../logger.js";
 import { client } from "../db/connections.js";
 
 export const healthCheck = async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-        await reply.code(200).send({ status: "ok" });
-    } catch (error: unknown) {
-        const err = error as Error;
-        logger.error(`Health check failed. Error: ${err.message}`);
-    } finally {
-        logger.info(`Status code health check : ${reply.statusCode}`);
-    }
+    await reply.code(200).send({ status: "ok" });
 };
 
 export const dbHealthCheck = async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-        if (client) {
-            await reply.code(200).send({ status: "Connected to db"});
-        } else {
-            await reply.code(400).send({error: "Not connected to db"});
-        }
-    } catch (error: unknown) {
-        const err = error as Error;
-        logger.error(`DB Health check failed. Error: ${err.message}`);
-    } finally {
-        logger.info(`Status code health db check : ${reply.statusCode}`);
+    if (client) {
+        await reply.code(200).send({ status: "Connected to db"});
+    } else {
+        await reply.code(400).send({error: "Not connected to db"});
     }
 }
