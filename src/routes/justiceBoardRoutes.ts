08e54f48a7538fdbc7d1f9f15d4ee286076251b8
Maor Krasner <maorkrasner02@gmail.com>
2024-03-02 19:24:45 +0200

    Add get justice-board by id route

--- src/routes/justiceBoardRoutes.ts
+++ src/routes/justiceBoardRoutes.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from "fastify";
 
-import { aggregateJusticeBoard } from "../db/justiceBoardFunctions.js";
+import { getJusticeBoard, getJusticeBoardById } from "../controllers/justiceBoardController.js";
 
 const justiceBoardRoutes = async (server: FastifyInstance) => {
-    server.get("/justice-board", aggregateJusticeBoard);
+    server.get("/justice-board", getJusticeBoard);
+    server.get("/justice-board/:id", getJusticeBoard);
 }
 
 export default justiceBoardRoutes;
