42a2970558cf548148d26e334dc05d143fd12561
Maor Krasner <maorkrasner02@gmail.com>
2024-03-02 18:53:10 +0200

    Add justice-board aggregation function

---
+++ src/db/justiceBoardFunctions.ts
@@ -0,0 +1,50 @@
+import { client } from "./connections.js";
+import type { Duty } from "../types/duty.js";
+import { aggregate } from "./operations.js";
+import type { justiceBoardElement } from "../types/justice-board.js";
+
+const dutiesCollectionName = "duties";
+
+export const aggregateJusticeBoard = async () => {
+const filter = [
+    {
+        $match: {
+            soldiers: { $exists: true, $not: { $size: 0 } }
+        }
+    },
+    {
+        $unwind: "$soldiers"
+    },
+    {
+        $lookup: {
+            from: "soldiers",
+            localField: "soldiers",
+            foreignField: "_id",
+            as: "soldierInfo"
+    }
+    },
+    {
+        $unwind: "$soldierInfo"
+    },
+    {
+        $project: {
+            soldierId: "$soldierInfo._id",
+            dutyValue: "$value"
+        }
+    },
+    {
+        $group: {
+            _id: "$soldierId",
+            totalDutyValue: { $sum: "$dutyValue" }
+        }
+    }
+]
+  
+    const aggregationArray = await aggregate<Duty & Document>(
+      client,
+      dutiesCollectionName,
+      filter
+    );
+  
+    return aggregationArray as justiceBoardElement[];
+};
