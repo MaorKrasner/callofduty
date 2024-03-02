6d8aea7ed5bc0151a2a2b7dd1277ab9e52bc41ce
Maor Krasner <maorkrasner02@gmail.com>
2024-03-02 19:01:00 +0200

    export justice-board routes

--- src/routes/justiceBoardRoutes.ts
+++ src/routes/justiceBoardRoutes.ts
@@ -5,3 +5,5 @@ import { aggregateJusticeBoard } from "../db/justiceBoardFunctions.js";
 const justiceBoardRoutes = async (server: FastifyInstance) => {
     server.get("/justice-board", aggregateJusticeBoard);
 }
+
+export default justiceBoardRoutes;
