ee277cd667e0fc5147140a86dd8e461421fdd5d9
maorkrasner <maorkrasner02@gmail.com>
2024-03-03 16:48:44 +0200

    Imporve controller functions

--- src/controllers/justiceBoardController.ts
+++ src/controllers/justiceBoardController.ts
@@ -1,31 +1,34 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 
-import { aggregateJusticeBoard, aggregateJusticeBoardById } from "../db/justiceBoardFunctions.js";
+import {
+  aggregateJusticeBoard,
+  aggregateJusticeBoardById,
+} from "../db/justiceBoardFunctions.js";
 import { findSoldier } from "../db/soldierDBFunctions.js";
 import type { justiceBoardElement } from "../types/justice-board.js";
 
 export const getJusticeBoard = async (
-    request: FastifyRequest,
-    reply: FastifyReply
+  request: FastifyRequest,
+  reply: FastifyReply
 ) => {
-    const justiceBoard: justiceBoardElement[] = await aggregateJusticeBoard();
+  const justiceBoard: justiceBoardElement[] = await aggregateJusticeBoard();
 
-    await reply.code(200).send(justiceBoard);
+  await reply.code(200).send(justiceBoard);
 };
 
 export const getJusticeBoardById = async (
-    request: FastifyRequest,
-    reply: FastifyReply
+  request: FastifyRequest,
+  reply: FastifyReply
 ) => {
   const { id } = request.params as { id: string };
   const soldier = await findSoldier(id);
 
   if (soldier) {
     const soldierScore = await aggregateJusticeBoardById(id);
-    await reply.code(200).send({score: soldierScore});
+    await reply.code(200).send({ score: soldierScore });
   } else {
     await reply
-      .code(400)
-      .send({error: `Couldn't find soldier with id ${id}`});
+      .code(404)
+      .send({ error: `Couldn't find soldier with id ${id}` });
   }
 };
