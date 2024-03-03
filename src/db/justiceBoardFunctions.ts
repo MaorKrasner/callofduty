8aa3934f86506ff457363e2f68f39bfbb1e4c7e3
maorkrasner <maorkrasner02@gmail.com>
2024-03-03 16:49:12 +0200

    Change logic of db functions

--- src/db/justiceBoardFunctions.ts
+++ src/db/justiceBoardFunctions.ts
@@ -6,66 +6,48 @@ import type { justiceBoardElement } from "../types/justice-board.js";
 const dutiesCollectionName = "duties";
 
 export const aggregateJusticeBoard = async () => {
-    const filter = [
-        {
-            $match: {
-                soldiers: { $exists: true, $not: { $size: 0 } }
-            }
-        },
-        {
-            $unwind: "$soldiers"
-        },
-        {
-            $lookup: {
-                from: "soldiers",
-                localField: "soldiers",
-                foreignField: "_id",
-                as: "soldierInfo"
-            }
-        },
-        {
-            $unwind: "$soldierInfo"
-        },
-        {
-            $project: {
-                soldierId: "$soldierInfo._id",
-                dutyValue: "$value"
-            }
-        },
-        {
-            $group: {
-                _id: "$soldierId",
-                totalDutyValue: { $sum: "$dutyValue" }
-            }
-        }
-    ]
-  
-    const aggregationArray = await aggregate<Duty & Document>(
-      client,
-      dutiesCollectionName,
-      filter
-    );
-  
-    return aggregationArray as justiceBoardElement[];
+  const filter: Object[] = [
+    {
+      $unwind: "$soldiers",
+    },
+    {
+      $group: {
+        _id: "$soldiers",
+        score: { $sum: "$value" },
+      },
+    },
+    {
+      $lookup: {
+        from: "soldiers",
+        localField: "_id",
+        foreignField: "_id",
+        as: "soldierDetails",
+      },
+    },
+    {
+      $unwind: "$soldierDetails",
+    },
+    {
+      $project: {
+        _id: "$soldierDetails._id",
+        score: "$score",
+      },
+    },
+  ];
+
+  const aggregationArray = await aggregate<justiceBoardElement & Document>(
+    client,
+    dutiesCollectionName,
+    filter
+  );
+
+  return aggregationArray as justiceBoardElement[];
 };
 
 export const aggregateJusticeBoardById = async (id: string) => {
-    const filter = [
-        {
-            $match: {
-                soldiers: id
-            }
-        },
-        {
-            $unwind: "$soldiers"
-        }
-    ];
+  const aggregationArray = await aggregateJusticeBoard();
+  const idArray = aggregationArray.map((element) => element._id);
+  const scoreArray = aggregationArray.map((element) => element.score);
 
-    const aggregationArray = await aggregate<Duty & Document>(
-      client,
-      dutiesCollectionName,
-      filter
-    ) as justiceBoardElement[];
-
-    return aggregationArray[0].score;
+  return !idArray.includes(id) ? 0 : scoreArray[idArray.indexOf(id)];
 };
