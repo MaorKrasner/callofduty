456f0956bf967140937e2512166673ddf4860723
maorkrasner <maorkrasner02@gmail.com>
2024-03-19 17:34:24 +0200

    Add test data for justice board

---
+++ tests/testData/justice-board.ts
@@ -0,0 +1,8 @@
+import { Soldier } from "../../src/types/soldier";
+
+export const justiceBoardTestSoldier: Partial<Soldier> = {
+  _id: "4567810",
+  name: "Test man",
+  rank: { name: "sergeant", value: 2 },
+  limitations: ["beard", "hair", "hatash7"],
+};
