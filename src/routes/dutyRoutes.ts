2ec3303f60324cfc8b6152974ac026511c25a3e9
maorkrasner <maorkrasner02@gmail.com>
2024-03-20 17:39:52 +0200

    Add routes for task5

--- src/routes/dutyRoutes.ts
+++ src/routes/dutyRoutes.ts
@@ -6,6 +6,8 @@ import {
   getDutiesByFilters,
   getDutyById,
   putConstraintsById,
+  putScheduleById,
+  putCancelById,
   updateDutyById,
 } from "../controllers/dutyController.js";
 
@@ -16,6 +18,8 @@ const dutyRoutes = async (server: FastifyInstance) => {
   server.delete("/duties/:id", deleteDutyById);
   server.patch("/duties/:id", updateDutyById);
   server.put("/duties/:id/constraints", putConstraintsById);
+  server.put("/duties/:id/schedule", putScheduleById);
+  server.put("/duties/:id/cancel", putCancelById);
 };
 
 export default dutyRoutes;
